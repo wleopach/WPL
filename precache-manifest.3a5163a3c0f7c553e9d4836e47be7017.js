6e21b0ad0f6a1a65b857f2819290c4dc2c599af3
unknown <s.carrero@uniandes.edu.co>
2021-11-29 23:34:10 -0500

    Updates

---
+++ precache-manifest.3a5163a3c0f7c553e9d4836e47be7017.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb9a22edabd4bbfbc3749d9731e1a48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45e64723df4d8d9c0371",
+    "url": "/static/css/2.f1cf0232.chunk.css"
+  },
+  {
+    "revision": "58f6108ec85f11e3639b",
+    "url": "/static/css/4.4cf50420.chunk.css"
+  },
+  {
+    "revision": "98e8010dd000443a4081",
+    "url": "/static/css/main.94dc57ee.chunk.css"
+  },
+  {
+    "revision": "45e64723df4d8d9c0371",
+    "url": "/static/js/2.049e3ecd.chunk.js"
+  },
+  {
+    "revision": "eca0aef68ad1fbfef95c",
+    "url": "/static/js/3.a7ec4ace.chunk.js"
+  },
+  {
+    "revision": "58f6108ec85f11e3639b",
+    "url": "/static/js/4.43a55f69.chunk.js"
+  },
+  {
+    "revision": "98e8010dd000443a4081",
+    "url": "/static/js/main.607b1ca9.chunk.js"
+  },
+  {
+    "revision": "a08ffd22e1fae8f3ecda",
+    "url": "/static/js/runtime-main.0d5ad3ee.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "80e52295922c70fdaa79d54afe26e883",
+    "url": "/static/media/bg-home.80e52295.jpg"
+  },
+  {
+    "revision": "058968437a6816318c07446758d51df7",
+    "url": "/static/media/img-1.05896843.jpg"
+  },
+  {
+    "revision": "8b073cf702bddf835869b171fe03d73d",
+    "url": "/static/media/img-1.8b073cf7.jpg"
+  },
+  {
+    "revision": "b2926a4cba8de7a66bc6d3e07c0b4682",
+    "url": "/static/media/img-2.b2926a4c.jpg"
+  },
+  {
+    "revision": "d0b8f70643b398774a14ebe4e56bbdcb",
+    "url": "/static/media/img-2.d0b8f706.jpg"
+  },
+  {
+    "revision": "23d0cae639e952805c67d63803ee81c8",
+    "url": "/static/media/img-3.23d0cae6.jpg"
+  },
+  {
+    "revision": "41b51894b16a8fa4cabb3190cd9efda0",
+    "url": "/static/media/img-4.41b51894.jpg"
+  },
+  {
+    "revision": "62c6c4cbe6933a556d9e4393434cca8c",
+    "url": "/static/media/img-5.62c6c4cb.jpg"
+  },
+  {
+    "revision": "07c9babb2e101fa952538a5cf85258c5",
+    "url": "/static/media/logo.07c9babb.png"
+  },
+  {
+    "revision": "1a95531b09ffc9b76104e0003b456a0e",
+    "url": "/static/media/materialdesignicons-webfont.1a95531b.woff"
+  },
+  {
+    "revision": "51b68d0604660b53c44400ccdef379c0",
+    "url": "/static/media/materialdesignicons-webfont.51b68d06.svg"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/static/media/materialdesignicons-webfont.6acc7d6c.ttf"
+  },
+  {
+    "revision": "84c6fe1e4353c916d8f1452e82bdef8d",
+    "url": "/static/media/materialdesignicons-webfont.84c6fe1e.woff2"
+  },
+  {
+    "revision": "be7691c114e7c9d0543b89e824c4003d",
+    "url": "/static/media/materialdesignicons-webfont.be7691c1.eot"
+  }
+]);
